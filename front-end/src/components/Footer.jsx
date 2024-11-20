7a14b4d449b28c9a2d6c6d00570ae6157672b6c1
ayuizu <ayuizu0519@gmail.com>
2024-11-19 22:44:46 -0300

    feat: added icons at ContactPage and Footer. Minor layout fixes

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,6 +1,14 @@
 import Logo from "../assets/logo.png";
 import classes from "./Footer.module.css";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faTelegram } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+
 function Footer() {
   return (
     <footer>
@@ -14,14 +22,20 @@ function Footer() {
               <li><a href="/">Home</a></li>
               <li><a href="/about">Sobre</a></li>
               <li><a href="/contact">Contato</a></li>
-              <li><a href="faq">FAQ</a></li>
+              <li><a href="/faq">FAQ</a></li>
             </ul>
           </div>
           <div className={classes.contentDiv}>
             <ul>
               <li><a href="/login">Login</a></li>
-              <li><a href="register">Cadastre-se</a></li>
+              <li><a href="/register">Cadastre-se</a></li>
             </ul>
+            <div className={classes.contactIconsDiv}>
+              <a href="#" className={classes.contactIcon}><FontAwesomeIcon icon={faWhatsapp} style={{color: "#1C4650",}} size="xl" /></a>
+              <a href="#" className={classes.contactIcon}><FontAwesomeIcon icon={faTelegram} style={{color: "#1C4650",}} size="xl" /> </a>
+              <a href="#" className={classes.contactIcon}><FontAwesomeIcon icon={faInstagram} style={{color: "#1C4650",}} size="xl" /> </a>
+              <a href="#" className={classes.contactIcon}><FontAwesomeIcon icon={faXTwitter} style={{color: "#1C4650",}} size="xl" /> </a>
+            </div>
           </div>
 
           <p className={classes.copyrightText}>
--- front-end/src/pages/ContactPage.jsx
+++ front-end/src/pages/ContactPage.jsx
@@ -1,6 +1,11 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import classes from "../pages/ContactPage.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faTelegram } from '@fortawesome/free-brands-svg-icons';
 
 const ContactPage = () => {
   return (
@@ -18,23 +23,27 @@ const ContactPage = () => {
 
         <div className={classes.contactInfo}>
           <div className={classes.contactItem}>
+            <div className={classes.contactIcon}><FontAwesomeIcon icon={faPhone} style={{color: "#1C4650"}} size="xs" /> </div>
             <h3>Telefone</h3>
             <p>0800 000 000 000</p>
           </div>
 
           <div className={classes.contactItem}>
+            <div className={classes.contactIcon}><FontAwesomeIcon icon={faWhatsapp} style={{color: "#1C4650",}} size="xs" /> </div>
             <h3>WhatsApp</h3>
             <p>(51) 3333-3333</p>
           </div>
 
           <div className={classes.contactItem}>
-            <h3>Email</h3>
+            <div className={classes.contactIcon}><FontAwesomeIcon icon={faEnvelope} style={{color: "#1C4650",}} size="xs" /> </div>
+            <h3>E-mail</h3>
             <p>
-              <a href="">cartaosaude@ig.com.br</a>
+            <a href="">contato@salutia.com</a>
             </p>
           </div>
 
           <div className={classes.contactItem}>
+            <div className={classes.contactIcon}><FontAwesomeIcon icon={faTelegram} style={{color: "#1C4650",}} size="xs" /> </div>
             <h3>Telegram</h3>
             <p>
               <a href="" target="_blank">
