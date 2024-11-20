0b885d2b4961c4e753e0572b74c4d40c4ea20a74
ayuizu <ayuizu0519@gmail.com>
2024-11-19 21:48:49 -0300

    feat: refactored ContactPage css

--- front-end/src/pages/ContactPage.jsx
+++ front-end/src/pages/ContactPage.jsx
@@ -1,14 +1,14 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import "../pages/ContactPage.module.css";
+import classes from "../pages/ContactPage.module.css";
 
 const ContactPage = () => {
   return (
-    <div className="page">
+    <div className={classes.page}>
       <div>
         <Header />
       </div>
-      <div class="page--contact">
+      <div class={classes.pageContact}>
         <h1>Entre em Contato</h1>
         <p>
           Estamos aqui para ajudar! Entre em contato conosco através dos nossos
@@ -16,25 +16,25 @@ const ContactPage = () => {
           buscar suporte, nossa equipe estará à disposição.
         </p>
 
-        <div class="contact-info">
-          <div class="contact-item">
+        <div class={classes.contactInfo}>
+          <div class={classes.contactItem}>
             <h3>Telefone</h3>
             <p>0800 000 000 000</p>
           </div>
 
-          <div class="contact-item">
+          <div class={classes.contactItem}>
             <h3>WhatsApp</h3>
             <p>(51) 3333-3333</p>
           </div>
 
-          <div class="contact-item">
+          <div class={classes.contactItem}>
             <h3>Email</h3>
             <p>
               <a href="">cartaosaude@ig.com.br</a>
             </p>
           </div>
 
-          <div class="contact-item">
+          <div class={classes.contactItem}>
             <h3>Telegram</h3>
             <p>
               <a href="" target="_blank">
