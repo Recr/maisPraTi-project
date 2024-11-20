9dbb59370b63fba9c64da14cbbbdfe4316ee240b
ayuizu <ayuizu0519@gmail.com>
2024-11-19 21:42:31 -0300

    feat: refactored UserPage css

--- front-end/src/pages/UserPage.jsx
+++ front-end/src/pages/UserPage.jsx
@@ -3,20 +3,21 @@
 import HeaderIn from "../components/HeaderIn";
 import Footer from "../components/Footer";
 import Menu from "../components/Menu";
-import "./UserPage.module.css"
+import classes from "./UserPage.module.css"
+
 const UserPage = () => {
     return (
-        <div className="page">
+        <div className={classes.page}>
             <div><HeaderIn /></div>
-            <div className="userContent">
-                <div className="div--menu">
+            <div className={classes.userContent}>
+                <div className={classes.divMenu}>
                     <Menu />
                 </div>
-                <div className="div--content">
-                    <div className="div--image">
-                        <img className="userImage" src='src/assets/users_pixabay.jpg' alt="Mãos unidas" />
+                <div className={classes.divContent}>
+                    <div className={classes.divImage}>
+                        <img className={classes.userImage} src='src/assets/users_pixabay.jpg' alt="Mãos unidas" />
                     </div>
-                    <div className="info"><h1>Bem-vindo(a) ao Health Card!</h1>
+                    <div className={classes.divInfo}><h1>Bem-vindo(a) ao Health Card!</h1>
                         <p>A plataforma digital que centraliza e protege todas as suas informações de saúde! Aqui, você pode acompanhar seu histórico médico, acessar resultados de exames, gerenciar vacinas, consultas, e muito mais, tudo de forma rápida e segura.
 
                            <br /><br /> O Health Card foi desenvolvido para oferecer mais autonomia e praticidade, facilitando o acesso e o compartilhamento de informações essenciais com profissionais de saúde sempre que necessário. Explore as funcionalidades e mantenha-se sempre atualizado(a) sobre sua saúde. Aproveite o futuro do cuidado digital!</p></div>
