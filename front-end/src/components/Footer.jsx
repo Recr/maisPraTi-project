b0c1b974e04a91c7d75080725f0afff92dba4288
ayuizu <ayuizu0519@gmail.com>
2024-11-19 22:05:28 -0300

    feat: re-fixed footer at the bottom of the page

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -9,17 +9,25 @@ function Footer() {
           <img src={Logo} alt="Health Card" />
         </div>
         <div className={classes.contentInfo}>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">Sobre</a></li>
-            <li><a href="/contact">Contato</a></li>
-            <li><a href="faq">FAQ</a></li>
-            <li><a href="/login">Login</a></li>
-            <li><a href="register">Cadastre-se</a></li>
-          </ul>
+          <div className={classes.contentDiv}>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">Sobre</a></li>
+              <li><a href="/contact">Contato</a></li>
+              <li><a href="faq">FAQ</a></li>
+            </ul>
+          </div>
+          <div className={classes.contentDiv}>
+            <ul>
+              <li><a href="/login">Login</a></li>
+              <li><a href="register">Cadastre-se</a></li>
+            </ul>
+          </div>
+
           <p className={classes.copyrightText}>
             Copyright © 2024 Salutia. Todos os direitos reservados.
           </p>
+          
         </div>
       </div>
     </footer>
