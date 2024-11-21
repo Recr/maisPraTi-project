05259864fbd03041ed8622468d32cd39fda2d6a1
Recr <126016150+Recr@users.noreply.github.com>
2024-11-21 09:39:11 -0300

    feat: sidebar design

--- front-end/src/components/Menu.jsx
+++ front-end/src/components/Menu.jsx
@@ -1,21 +1,34 @@
 // Falta ajustar os links
 
 import classes from "./Menu.module.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStethoscope, faCapsules, faSyringe, faHeartPulse, faWeightScale, faCalendar } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const SidebarLink = styled(NavLink)`
+  text-decoration: none;
+  color: #333;
+  padding: 10px;
+  display: block;
+
+  &.active {
+    color: var(--color1);
+    font-size: 1.125rem;
+  }
+`;
 
 const Menu = () => {
   return (
     <div className={classes.menuContainer}>
       <h2>Meus Dados de Saúde</h2>
       <ul>
-        <li><Link className="header--link" to="/symptoms"><FontAwesomeIcon className={classes.icon} icon={faStethoscope} /> Sintomas</Link></li>
-        <li><Link className="header--link" to="/medicines"><FontAwesomeIcon className={classes.icon} icon={faCapsules} /> Medicamentos</Link></li>
-        <li><Link className="header--link" to="/vaccines"><FontAwesomeIcon className={classes.icon} icon={faSyringe} /> Vacinas</Link></li>
-        <li><Link className="header--link" to="/bloodPressure"><FontAwesomeIcon className={classes.icon} icon={faHeartPulse} /> Pressão arterial</Link></li>
-        <li><Link className="header--link" to="/weight"><FontAwesomeIcon className={classes.icon} icon={faWeightScale} /> Peso</Link></li>
-        <li><Link className="header--link" to="/appointments"><FontAwesomeIcon className={classes.icon} icon={faCalendar} /> Consultas</Link></li>
+        <li><SidebarLink className={({ isActive }) => isActive ? 'active' : ''} to="/symptoms"><FontAwesomeIcon className={classes.icon} icon={faStethoscope} /> Sintomas</SidebarLink></li>
+        <li><SidebarLink className={({ isActive }) => isActive ? 'active' : ''} to="/medicines"><FontAwesomeIcon className={classes.icon} icon={faCapsules} /> Medicamentos</SidebarLink></li>
+        <li><SidebarLink className={({ isActive }) => isActive ? 'active' : ''} to="/vaccines"><FontAwesomeIcon className={classes.icon} icon={faSyringe} /> Vacinas</SidebarLink></li>
+        <li><SidebarLink className={({ isActive }) => isActive ? 'active' : ''} to="/bloodPressure"><FontAwesomeIcon className={classes.icon} icon={faHeartPulse} /> Pressão arterial</SidebarLink></li>
+        <li><SidebarLink className={({ isActive }) => isActive ? 'active' : ''} to="/weight"><FontAwesomeIcon className={classes.icon} icon={faWeightScale} /> Peso</SidebarLink></li>
+        <li><SidebarLink className={({ isActive }) => isActive ? 'active' : ''} to="/appointments"><FontAwesomeIcon className={classes.icon} icon={faCalendar} /> Consultas</SidebarLink></li>
       </ul>
     </div>
   );
